a79a047d0b5f8e7843bf1ee4d28c88c584701e7f
Temur <shernazarovtemurcom001@gmail.com>
2024-01-09 05:01:43 +0500

    I wanna sleep�️

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddContact from "./components/AddContact";
 
 const App = () => {
+  const [person, setperson] = useState({
+    firstName: "Tom",
+    lastName: "Jerry",
+    number: "+998990010099",
+  });
+
   return (
     <>
       <h1>
-        <div className="container pt-4">Hello, my sleepers 🛌 😴 💤 !</div>
+        <div className="container pt-4 text-center pb-3">
+          Registration of Students 🧑‍🎓 👩‍🎓!
+        </div>
+        <AddContact />
       </h1>
     </>
   );
